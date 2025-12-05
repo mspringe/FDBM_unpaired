09d9651b41cc1b2c0923d7b0e633aee5632f1f11
springenberg <maximilian.springenberg@hhi.fraunhofer.de>
2025-12-05 07:11:13 -0800

    documentation

--- afhq_download.sh
+++
@@ -1,8 +0,0 @@
-URL=https://www.dropbox.com/s/t9l9o3vsx2jai3z/afhq.zip?dl=0
-ZIP_FILE=./afhq.zip
-mkdir -p ./datasets/AFHQ
-wget -N $URL -O $ZIP_FILE
-unzip $ZIP_FILE -d ./datasets/AFHQ
-mv ./datasets/AFHQ/afhq/* ./datasets/AFHQ/
-rmdir ./datasets/AFHQ/afhq/
-rm $ZIP_FILE
